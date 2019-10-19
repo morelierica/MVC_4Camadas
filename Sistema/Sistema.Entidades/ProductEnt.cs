d1b5412f0f878d7927f188323ef36567ec654a48
Erica Moreli <ericammoreli@gmail.com>
2019-10-19 10:22:10 -0300

    Tela de produtos

--- Sistema/Sistema.Entidades/ProductEnt.cs
+++ Sistema/Sistema.Entidades/ProductEnt.cs
@@ -4,7 +4,16 @@ using System.Text;
 
 namespace Sistema.Entidades
 {
-    class ProductEnt
+    public class ProductEnt
     {
+        private int pro_id;
+        private string pro_name;
+        private string pro_description;
+        private double pro_price;
+
+        public int Pro_id { get => pro_id; set => pro_id = value; }
+        public string Pro_name { get => pro_name; set => pro_name = value; }
+        public string Pro_description { get => pro_description; set => pro_description = value; }
+        public double Pro_price { get => pro_price; set => pro_price = value; }
     }
 }
